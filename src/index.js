46e8e70eeadeb904db76eeb4841508272c9e866e
bybybybybyby <bybybybybyby@users.noreply.github.com>
2021-02-22 16:50:53 -0800

    Add Expressions.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Bro";
+const lName = "Ski";
+const num = 8;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+
+    {/* Same result as above */}
+    <h1>Hello {fName + " " + lName}!</h1>
+
+    <p>Your lucky number is {num}!</p>
+
+    {/* Use any expression */}
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}!</p>
+  </div>,
+  document.getElementById("root")
+);
